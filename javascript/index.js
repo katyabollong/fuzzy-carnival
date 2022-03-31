05855ebba30cc9aa890264343cfddba7d3288b0c
katyabollong <97697240+katyabollong@users.noreply.github.com>
2022-03-31 11:28:49 -0600

    css
    css

---
+++ javascript/index.js
@@ -0,0 +1,9 @@
+function windowLoaded() {
+  document.getElementById('button-1').classList.toggle('is-red');
+}
+
+function windowLoaded() {
+  document.getElementById('button-1').addEventListener('click', buttonClicked);
+}
+
+window.onload = windowLoaded;
