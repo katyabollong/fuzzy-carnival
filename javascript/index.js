e7ee20892cd62a7a2e5fbd673e94fab0084f3dfa
katyabollong <97697240+katyabollong@users.noreply.github.com>
2022-03-31 11:46:03 -0600

    java
    java

--- javascript/index.js
+++ javascript/index.js
@@ -7,3 +7,16 @@ function windowLoaded() {
 }
 
 window.onload = windowLoaded;
+
+const para = document.querySelector('p');
+para.addEventListener('click', updateName);
+
+function updateName(){
+  const name = prompt('Enter a new name');
+  Para.textContent= `Player1:${Name}`;
+}
+
+let myVariable;
+myVariable = 'katya';
+
+let myVariable2 = 'brooke';
