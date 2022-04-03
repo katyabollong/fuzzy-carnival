7f84d33daa1efa6796eab9dd494b23934cd3ad36
katyabollong <97697240+katyabollong@users.noreply.github.com>
2022-04-03 12:43:52 -0600

    js
    js

--- javascript/index.js
+++ javascript/index.js
@@ -28,3 +28,20 @@ if (iceCream==='chocolate') {
 } else {
   Alert ('aww but chocolate is my favorite');
 }
+
+let btn1 = document.querySelector('#btn-1');
+let btn2 = document.querySelector('#btn-2');
+let btn3 = document.querySelector('#btn-3');
+let btn4 = document.querySelector('#btn-4');
+btn1.addEventListener('click', () =>{
+    document.body.style.backgroundImage = "url('images/1.jpg')";
+});
+btn2.addEventListener('click', () =>{
+    document.body.style.backgroundImage = "url('images/2.jpg')";
+});
+btn3.addEventListener('click', () =>{
+    document.body.style.backgroundImage = "url('images/3.jpg')";
+});
+btn4.addEventListener('click', () =>{
+    document.body.style.backgroundImage = "url('https://i.imgur.com/mPKfD2J.jpg')";
+});
